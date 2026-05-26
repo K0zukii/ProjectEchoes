8668e46724f265671afb1737624be703d5cbbcf8
Aymen Embarki <aymenembarki@gmail.com>
2026-05-27 00:12:51 +0200

    Add PlayerStamina and integrate with states
    Introduce a PlayerStamina component and wire stamina logic into the player state machine and movement states. PlayerStamina exposes serialized settings (maxStamina, drainRate, regenRate, timeBeforeRegen, sprintThreshold), tracks current stamina, provides ConsumeStamina() and RegenerateStamina(), and a canSprint flag. State constructors and PlayerStateMachine were updated to pass and cache PlayerStamina; SprintState now consumes stamina and stops sprinting when canSprint is false, WalkState and CrouchState call RegenerateStamina. The Game scene gains a PlayerStamina component with default values (max 100, drain 15, regen 10, timeBeforeRegen 3, sprintThreshold 20).

--- Project Echoes/Assets/Scripts/Player/PlayerBasics/PlayerStamina.cs
+++ Project Echoes/Assets/Scripts/Player/PlayerBasics/PlayerStamina.cs
@@ -2,15 +2,55 @@ using UnityEngine;
 
 public class PlayerStamina : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float maxStamina;
+    [SerializeField] private float drainRate;
+    [SerializeField] private float regenRate;
+    [SerializeField] private float timeBeforeRegen = 3f;
+    [SerializeField] private float sprintThreshold = 20f;
+    private float regenTimer = 0f;
+    public bool canSprint {get; private set;}
+    private float _currentStamina;
+    public float currentStamina
     {
-        
+        get {return _currentStamina;}
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
+        _currentStamina = maxStamina;
+        canSprint = true;
+    }
+
+    public void ConsumeStamina()
+    {
+        _currentStamina -= drainRate * Time.deltaTime;
+        _currentStamina = Mathf.Clamp(_currentStamina, 0, maxStamina);
+
+        if (_currentStamina <= 0)
+        {
+            _currentStamina = 0;
+            canSprint = false;
+            regenTimer = timeBeforeRegen;
+        }
+    }
+
+    public void RegenerateStamina()
+    {
+        if (regenTimer > 0)
+        {
+            regenTimer -= Time.deltaTime;
+            return;
+        }
+        else
+        {
+            _currentStamina += regenRate * Time.deltaTime;
+            _currentStamina = Mathf.Clamp(_currentStamina, 0, maxStamina);
+
+            if (canSprint == false && currentStamina >= sprintThreshold)
+            {
+                canSprint = true;
+            }
+        }
         
     }
 }
--- Project Echoes/Assets/Scripts/Player/States/WalkState.cs
+++ Project Echoes/Assets/Scripts/Player/States/WalkState.cs
@@ -5,12 +5,14 @@ public class WalkState : IState
     private PlayerStateMachine playerState;
     private PlayerMovement playerMovement;
     private PlayerInputHandler playerInput;
+    private PlayerStamina playerStamina;
 
-    public WalkState(PlayerStateMachine stateMachine, PlayerMovement movement, PlayerInputHandler input)
+    public WalkState(PlayerStateMachine stateMachine, PlayerMovement movement, PlayerInputHandler input, PlayerStamina stamina)
     {
         playerState = stateMachine;
         playerMovement = movement;
         playerInput = input;
+        playerStamina = stamina;
     }
 
     public void OnEnterState()
@@ -20,14 +22,15 @@ public class WalkState : IState
     public void UpdateState()
     {
         playerMovement.Move(playerInput.MoveInput, 3.5f);
+        playerStamina.RegenerateStamina();
 
-        if (playerInput.IsSprinting)
+        if (playerInput.IsSprinting && playerStamina.canSprint)
         {
-            playerState.ChangeState(new SprintState(playerState, playerMovement, playerInput));
+            playerState.ChangeState(new SprintState(playerState, playerMovement, playerInput, playerStamina));
         }
         else if (playerInput.IsCrouching)
         {
-            playerState.ChangeState(new CrouchState(playerState, playerMovement, playerInput));
+            playerState.ChangeState(new CrouchState(playerState, playerMovement, playerInput, playerStamina));
         }
     }
     public void ExitState() {}
